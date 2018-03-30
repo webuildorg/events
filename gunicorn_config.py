10bcbe6efdd248328e56601afb74b247eb7cca19
alyssaq <alyssaquek@gmail.com>
2018-03-30 20:56:32 +0800

    gunicorn settings

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
-threads = int(os.environ.get('GUNICORN_THREADS', '5'))
+import multiprocessing
+workers = int(os.environ.get('GUNICORN_PROCESSES', multiprocessing.cpu_count()))
+threads = int(os.environ.get('GUNICORN_THREADS', 4))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
