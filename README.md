05897849299ac7c920241387af1b744d85d7784d
alyssaq <alyssaquek@gmail.com>
2018-04-15 16:36:47 +0800

    clarify

--- README.md
+++ README.md
@@ -5,13 +5,11 @@ Tested on Python 3.6+
 Subscribe to the ics calendar: http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/cal   
 List of events: http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/events   
 List of groups: http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/groups   
-
 List of filtered groups: http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/filtered_groups   
-List of filtered events: http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/filtered_events
 
 ### Development
 ```
-pip install -r requirements.txt
+$ pip install -r requirements.txt
 ```
 
 ### Run
--- config.py
+++ config.py
@@ -9,11 +9,26 @@ business_blacklist_tokens = frozenset([
     'business', 'enterprise', 'entrepreneur', 'entrepreneurship',
     'executive', 'founder', 'investor'])
 
+# Minimum number of tokens to blacklist a group or event in each topic [blockchain, bussines]
+token_thresholds = [1, 4]
 
-multi_blacklist_tokens = [
-    blockchain_blacklist_tokens,
-    business_blacklist_tokens
-]
+meetup = {
+    'groups_url': 'https://api.meetup.com/find/groups',
+    'events_url': 'https://api.meetup.com/2/events',
+    'max_event_hours': 12,
+    'max_meetup_responses': 150,
+    'display_time_format': '%d-%b, %a, %I:%M %p',
+
+    'params': {
+        'key': os.environ['MEETUP_API_KEY'],
+        'country': 'SG',
+        'location': 'Singapore',
+        'category': 34
+    }
+}
+
+########################
+# auto-enhancements to the blacklist tokens.
 
 # Enhance the blacklist tokens by expanding the set with possible typos
 enhanced_blockchain_blacklist_tokens = set()
@@ -33,26 +48,8 @@ multi_enhanced_blacklist_tokens = [
     enhanced_blockchain_blacklist_tokens,
     enhanced_business_blacklist_tokens
 ]
+blacklist_tokens = frozenset(list(blockchain_blacklist_tokens) + list(business_blacklist_tokens))
 
 print('{} blacklist tokens'.format(
     len(enhanced_blockchain_blacklist_tokens) + len(enhanced_business_blacklist_tokens)))
 
-# Minimum number of tokens to blacklist a group or event in each topic [blockchain, bussines]
-token_thresholds = [1, 4]
-
-blacklist_tokens = frozenset(list(blockchain_blacklist_tokens) + list(business_blacklist_tokens))
-
-meetup = {
-    'groups_url': 'https://api.meetup.com/find/groups',
-    'events_url': 'https://api.meetup.com/2/events',
-    'max_event_hours': 12,
-    'max_meetup_responses': 150,
-    'display_time_format': '%d-%b, %a, %I:%M %p',
-
-    'params': {
-        'key': os.environ['MEETUP_API_KEY'],
-        'country': 'SG',
-        'location': 'Singapore',
-        'category': 34
-    }
-}
