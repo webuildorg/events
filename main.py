567617e17b6aec284cac845fe8b5a27cef49ecb4
alyssaq <alyssaquek@gmail.com>
2018-10-03 10:32:56 +0800

    Print and serve on port 8080 by default

--- main.py
+++ main.py
@@ -191,4 +191,6 @@ def favicon():
 
 if __name__ == "__main__":
     app = run()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 8080))
+    print('Serving on port {}'.format(port))
+    app.run(host='0.0.0.0', port=port)
